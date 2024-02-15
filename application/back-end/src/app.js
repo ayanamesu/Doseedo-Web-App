d0c2a6fa136545eafeb6f0377128c34821b5f081
ayanamesu <wlee17@sfsu.edu>
2024-02-15 14:54:02 -0800

    created routes and router folder and files

--- application/back-end/src/app.js
+++ application/back-end/src/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 //this is to be discussed
 const port = process.env.port || 5173; 
-const router = require("./src/routes/router");
+const router = require("./routes/router");
 
 app.use(express.json());
 app.use("/router", router);
