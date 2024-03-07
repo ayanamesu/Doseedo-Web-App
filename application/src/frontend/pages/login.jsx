0dca62270a524f79caa537d2bcc688d406199a15
ayanamesu <wlee17@sfsu.edu>
2024-03-07 00:41:26 -0800

    bare bones for the login/signup functionality to test routing for back end using react query

--- application/src/frontend/pages/login.jsx
+++ application/src/frontend/pages/login.jsx
@@ -1,9 +1,23 @@
 const LoginPage = () => {
     return (
         <>
-        <p>This is the login page</p>
+            <div className="Login-box">
+                <h2>Login</h2>
+                
+               
+                <form action="/index" method="GET">
+                    <input type="text" placeholder="Phone, email or username" />
+                    <input type="password" placeholder="Password" />
+                    {/* 
+                        this is a quick test to see functionality - wing can change later
+                    */}
+                    <button type="submit">Login</button>
+                </form>
+                <button>Forgot password?</button>
+                <p>Don't have an account? <a id="signup-link" href="/signup">Sign up</a></p>
+            </div>
         </>
-    )
+    );
 };
 
 export default LoginPage;
--- application/src/frontend/pages/signUp.jsx
+++ application/src/frontend/pages/signUp.jsx
@@ -1,9 +1,28 @@
 const SignUpPage = () => {
     return (
         <>
-        <p>This is the SignUpPage</p>
+            <div className="signup-box">
+                <h2>Sign Up</h2>
+                
+               
+                <form action="/index" method="GET">
+                    <input type="text" placeholder="First name" />
+                    <input type="text" placeholder="Last name" />
+                    <input type="text" placeholder="Phone, email or username" />
+                    <input type="password" placeholder="Password" />
+                    <input type="password" placeholder="Confirm Password" />
+                    {/* 
+                        this is a quick test to see functionality - wing can change later
+                    */}
+                    <div className="btn-field">
+                    <button type="submit">Register</button>
+                    <button type="button" onClick={() => window.location.href = '/login'}>Login</button>
+                    </div>
+                </form>
+                
+            </div>
         </>
-    )
+    );
 };
 
 export default SignUpPage;
