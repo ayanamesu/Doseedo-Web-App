dfa01d920a919eb31ce3cd1362674aaf819403b2
ayanamesu <wlee17@sfsu.edu>
2024-03-17 23:48:53 -0700

    m2 complete front and back end working perfect

--- application/src/backend/server.js
+++ application/src/backend/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 var mysql = require("mysql2/promise");
 const cors = require("cors");
 
