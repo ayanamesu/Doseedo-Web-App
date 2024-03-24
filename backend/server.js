f4a1b4619a7ff70f6fe3e7a7f93611072960b396
ayanamesu <wlee17@sfsu.edu>
2024-03-23 19:03:26 -0700

    testing put for insertdb

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,11 @@
 const express = require("express");
-const app = express();
+
 const port = process.env.PORT || 8000;
 var mysql = require("mysql2/promise");
 const cors = require("cors");
 
-
+const app = express();
+app.use(express.json());
 app.use(cors());
 
 app.get("/dbtest", async (req, res) => {
@@ -19,6 +20,19 @@ app.get("/dbtest", async (req, res) => {
     res.json(data);
   } catch (error) {
     res.status(500).json({ error: "An error occurred" });
+  }
+});
+
+app.post("/dbtest2", async (req, res) => {
+  console.log(req.body);
+  
+  try {
+    const db = await connectToDB();
+    inserttest(db, req);
+    return res.send("fuck yes");
+  
+  } catch (error) {
+    return res.status(500).json({ error: "An error occurred" });
   }
 });
 
@@ -45,6 +59,7 @@ app.listen(port, () => {
   console.log(`Server is listening at http://${port}`);
 });
 
+
 //my sql database
 
 async function connectToDB() {
@@ -70,6 +85,7 @@ async function selecttest() {
   try {
     const query = "SELECT * FROM User;";
     const [result, fields] = await db.execute(query);
+    db.end();
     return result;
   } catch (error) {
     console.error(error);
@@ -82,51 +98,30 @@ async function selectmedicine() {
   try {
     const query = "SELECT * FROM Prescription;";
     const [result, fields] = await db.execute(query);
+    db.end();
     return result;
   } catch (error) {
     console.error(error);
     throw error;
   }
 }
-function inserttest(db) {
-  //   console.log("Testing insert into db");
+function inserttest(db, req) {
+    console.log("Testing insert into db");
   const reqBody = {
-    first_name: req.body.fname,
-    last_name: req.body.lname,
+    first_name: req.body.firstName,
+    last_name: req.body.lastName,
     email: req.body.email,
     password: req.body.password,
-    address_1: req.body.address1,
-    state: req.body.state,
-    city: req.body.city,
-    zip_code: req.body.zipCode,
-    phone: req.body.phone,
-  };
-  const insertQuery = `INSERT INTO User (first_name, last_name, email, password, address_1, state, city, zip_code, phone) 
-                       VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)`;
+  }; 
+  const insertQuery = `INSERT INTO user (first_name, last_name, email, password) 
+                       VALUES (?, ?, ?, ?)`;
   db.query(insertQuery, [
     reqBody.first_name,
     reqBody.last_name,
     reqBody.email,
     reqBody.password,
-    reqBody.address_1,
-    reqBody.state,
-    reqBody.city,
-    reqBody.zip_code,
-    reqBody.phone,
   ]);
-
-  res.send("Successfully inserted into table!");
 }
-
-// function alter_test(db, id, name) {
-//   db.query(`UPDATE test SET name=(?) WHERE id=(?)`, [name, id], function(err) {
-//     if (err) {
-//       console.error('Error executing query: ' + err.stack);
-//       return;
-//     }
-//     console.log("Successfully updated table!");
-//   })
-// }
 
 (async function main() {
   try {
