a4ca93ba8027332835daa20021a8007becea9dbc
jager47X <jager47xd@gmail.com>
2024-04-09 12:42:49 -0700

    rendersidebar

--- application/src/frontend/Components/Sidebar.jsx
+++ application/src/frontend/Components/Sidebar.jsx
@@ -1,36 +1,80 @@
-import React, { useState } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import "../App.css";
 import { SidebarData } from "./SidebarData";
-
+import { Navigate } from 'react-router-dom';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+ 
 function Sidebar() {
+  const [isLoggedIn, setLoginStatus] = useState(false);
+  const [isHomepage, setHomepage] = useState(false);
   const [sidebar, setSidebar] = useState(false);
   const showSidebar = () => setSidebar(!sidebar);
 
   const [iconClicked, setIconClicked] = useState(false);
-
+ 
+React.useEffect(() => {
+  /*
+  axios.get('http://localhost:8000/api/settings')
+  .then((res) => res.json())
+  .then((apiRes) => {
+    console.log(apiRes);
+    
+    setProducts(apiRes.data);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
+    axios.get('http://localhost:8000/api/session')
+  .then((res) => res.json())
+  .then((apiRes) => {
+    console.log(apiRes);
+    
+    setProducts(apiRes.data);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
+*/
+const isHomepage = () => {
+  return window.location.pathname === "/";
+};
+setHomepage(isHomepage());
+}, []);
+  //fetchData
   const handleClick = () => {
     showSidebar();
     setIconClicked(!iconClicked);
   };
+ 
+  const renderSidebar = () => {
+    if (!isLoggedIn || isHomepage) {
+      return (
+        <div className="navbar">
+          <img
+            src={
+              iconClicked
+                ? "https://cdn2.iconfinder.com/data/icons/media-controls-5/100/close-512.png"
+                : "https://cdn-icons-png.freepik.com/512/9293/9293128.png"
+            }
+            className="menu-icon"
+            onClick={handleClick}
+            alt="Menu"
+          />
+          <form className="search-bar" action="/dbtest" method="GET">
+            <input type="text" placeholder="Search..." name="search" />
+            <button type="submit">search</button>
+          </form>
+        </div>
+      );
+    } else {
+      return null; // Return null if the condition is not met
+    }
+  };
   return (
     <div>
-      <div className="navbar">
-        <img
-          src={
-            iconClicked
-              ? "https://cdn2.iconfinder.com/data/icons/media-controls-5/100/close-512.png"
-              : "https://cdn-icons-png.freepik.com/512/9293/9293128.png"
-          }
-          className="menu-icon"
-          onClick={handleClick}
-          alt="Menu"
-        />
-        <form className="search-bar" action="/dbtest" method="GET">
-          <input type="text" placeholder="Search..." name="search" />
-          <button type="submit">search</button>
-        </form>
-      </div>
+      {renderSidebar}
       <nav className={sidebar ? "sidebar active" : "sidebar"}>
         <ul className="sidebarList">
           {SidebarData.map((link, index) => (
@@ -48,6 +92,7 @@ function Sidebar() {
           ))}
         </ul>
       </nav>
+
     </div>
   );
 }
--- application/src/frontend/pages/home.jsx
+++ application/src/frontend/pages/home.jsx
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 
 const HomePage = () => {
     const [isLoginForm, setIsLoginForm] = useState(false);
-
+ 
     const handleFormSwitch = () => {
         setIsLoginForm(!isLoginForm);
     };
