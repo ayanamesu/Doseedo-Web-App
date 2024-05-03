96562e3a9c5cb10ffcd3fbc78c7be9a18b3558ff
leileigoose <111732708+leileigoose@users.noreply.github.com>
2024-05-03 15:28:03 -0700

    Updated all return all res in server.js. Did some unit tests for backend. Fixed emergency contact add query

--- backend/server.js
+++ backend/server.js
@@ -37,7 +37,7 @@ app.use(session({
 
 /** LOGIN
  * Frontend req: email, password
- * Backend res: Status code, session_id, user_id , account_type
+ * Backend res: Status code, session_id, user_id, account_type
  * Postman Check - SUCCESS
  */
 app.post('/login', async (req, res) => {
@@ -61,22 +61,20 @@ app.post('/login', async (req, res) => {
 
         const session_creation = await createSession(req.session.id, user_id, device); // stores session in our db
 
-        // if session creation was successful
         if (session_creation) {
           // req.session.id will return the session id to the frontend to create a cookie
-          // We can add to this if we like
-          res.status(200).json({session_id: req.session.id, user_id: user_id,user_accountType: userAccType}); 
+          return res.status(200).json({session_id: req.session.id, user_id: user_id, user_accountType: userAccType}); 
         } else {
-          res.status(500);
+          return res.status(500);
         }
 
       } else {
-        res.status(403).json({ msg: "Wrong email or password :(" }); // Error handling style needs to be done in the front end
+        return res.status(403).json({ msg: "Wrong email or password :(" }); // Error handling style needs to be done in the front end
       }
     }
   } catch (error) {
     console.error(error);
-    res.status(500).json({ "error": "Internal server error" });
+    return res.status(500).json({ "error": "Internal server error" });
   }
 });
 
@@ -100,8 +98,7 @@ app.post("/register", async (req, res) => {
     // If the user already has an account, redirect to the login page
     if (user_id) {
       console.log("User already has an account!");
-      res.status(200).json({msg: "User already has an account!"});
-      return;
+      return res.status(200).json({msg: "User already has an account!"});
     } else {
       // hash the password before storing it on the database
       const hash_pwd = await bcrypt.hash(password, 10);
@@ -110,14 +107,14 @@ app.post("/register", async (req, res) => {
       const [results, fields] =  await db.query(insertQuery, [first_name, last_name, email, hash_pwd, account_type]);
       
       if (results && results.affectedRows == 1) {
-        res.status(201).json({msg: "ACCOUNT SUCCESSFULLY CREATED"});
+        return res.status(201).json({msg: "ACCOUNT SUCCESSFULLY CREATED"});
       } else {
-        res.status(500).json({ "error": "Account creation failed" });
+        return res.status(500).json({ "error": "Account creation failed" });
       }
     }
   } catch (error) {
     console.error(error);
-    res.status(500).json({ "error": "Internal server error" });
+    return res.status(500).json({ "error": "Internal server error" });
   }
 });
 
@@ -138,9 +135,9 @@ app.post("/session", async (req, res) => {
     const [results, fields] = await db.query(query, [req.body.session_id]);
 
     if (results && results.length == 1 && !results[0].logout_time) {
-      res.status(200).json({user_id: results[0].user_id, session_id: req.body.session_id});
+      return res.status(200).json({user_id: results[0].user_id, session_id: req.body.session_id});
     } else {
-      res.status(401).json({ msg: "No session for this user"});
+      return res.status(401).json({ msg: "No session for this user"});
     }
   } catch (error) {
     console.error(error);
@@ -163,9 +160,9 @@ app.post('/profile', async (req, res) => {
     const [results, fields] = await db.query(query, [req.body.user_id]);
 
     if (results && results.length == 1) {
-      res.status(200).json(results[0]);
+      return res.status(200).json(results[0]);
     } else {
-      res.status(401).json({ msg: ""});
+      return res.status(401).json({ msg: "Invalid User ID"});
     }
   } catch (error) {
     console.error(error);
@@ -202,9 +199,9 @@ app.post('/profile/edit', async (req, res) => {
 
     const [results, fields] = await db.execute(query);
     if (results && results.affectedRows === 1) {
-      res.status(200).json({ msg: "Update successful!"});
+      return res.status(200).json({ msg: "Update successful!"});
     } else {
-      res.status(500).json({ msg: "Something went wrong when updating information"});
+      return res.status(500).json({ msg: "Something went wrong when updating information"});
     }
   } catch (error) {
     console.error(error);
@@ -229,9 +226,9 @@ app.post('/showcaregivers', async (req, res) => {
 
     if (results && results.length >= 1) {
       console.log(results);
-      res.status(200).json(results);
+      return res.status(200).json(results);
     } else {
-      res.status(204).json({ msg: "No caregivers for this user"});
+      return res.status(204).json({ msg: "No caregivers for this user"});
     }
   } catch (error) {
     console.error(error);
@@ -239,6 +236,34 @@ app.post('/showcaregivers', async (req, res) => {
   }
 });
 
+/** Patient List
+ * Show the list of patients for a caregiver
+ * Frontend req: user_id
+ * Backend res: Status code, all user information of patient(s) (first_name, last_name, email, phone, address_1, address_2, state, city, zip_code)
+ * Postman Check - SUCCESS
+ */ 
+app.post("/showpatients", async (req, res) => {
+  console.log("Show patients api")
+  console.log(req.body)
+  if (!req.body.user_id){
+    return res.status(400).json({ msg: "Missing the user_id"});
+  }
+
+  try {
+    const query = "SELECT user.* FROM account_link JOIN user ON account_link.patient_id = user.id WHERE account_link.caregiver_id = ?;";
+    const [results, fields] = await db.query(query, [req.body.user_id]);
+
+    if (results && results.length >= 1) {
+      console.log(results);
+      return res.status(200).json(results);
+    } else {
+      return res.status(204).json({ msg: "No patients for this user"});
+    }
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+});
 
 /** Link Accounts
  * Links an account to another account
@@ -256,34 +281,34 @@ app.post('/linkAccounts', async (req, res) => {
       const userAccType = await getAccountType(user_id);
       const newID = await hasAccount(email);
       if (!newID) {
-        res.status(400).json({ msg: "Incorrect email given"});
+        return res.status(400).json({ msg: "Incorrect email given"});
       }
       const addAccType = await getAccountType(newID);
 
       console.log("userAcctType: " + userAccType + "\taddAccType: " + addAccType);
 
       if (userAccType === addAccType) {
-        res.status(400).json({ msg: "Cannot link two of the same type accounts"})
+        return res.status(400).json({ msg: "Cannot link two of the same type accounts"})
       }
 
       if (userAccType === 'caregiver') {
         const insertQuery = "INSERT INTO account_link (caregiver_id, patient_id) VALUES (?, ?);";
         const [results, fields] = await db.query(insertQuery, [user_id, newID]);
         if (results && results.affectedRows == 1) {
-          res.status(201).json({ msg: "Link successful!"});
+          return res.status(201).json({ msg: "Link successful!"});
         } else {
-          res.status(500).json({ msg: "Something went wrong when linking accounts"});
+          return res.status(500).json({ msg: "Something went wrong when linking accounts"});
         }
       } else if (userAccType === 'patient') {
         const insertQuery = "INSERT INTO account_link (caregiver_id, patient_id) VALUES (?, ?);";
         const [results, fields] = await db.query(insertQuery, [newID, user_id]);
         if (results && results.affectedRows == 1) {
-          res.status(201).json({ msg: "Link successful!"});
+          return res.status(201).json({ msg: "Link successful!"});
         } else {
-          res.status(500).json({ msg: "Something went wrong when linking accounts"});
+          return res.status(500).json({ msg: "Something went wrong when linking accounts"});
         }
       } else {
-        res.status(500).json({ msg: "Incorrect account_type given"});
+        return res.status(500).json({ msg: "Incorrect account_type given"});
       }  
     } catch(error) {
       console.error(error);
@@ -429,9 +454,9 @@ app.post("/addmedicine", async (req, res) => {
     const [results, fields] = await db.query(query, values);
 
     if (results && results.affectedRows === 1) {
-      res.status(200).json({ msg: "Update successful!"});
+      return res.status(200).json({ msg: "Update successful!"});
     } else {
-      res.status(500).json({ msg: "Something went wrong when updating information"});
+      return res.status(500).json({ msg: "Something went wrong when updating information"});
     }
   } catch (error) {
     console.error(error);
@@ -457,13 +482,13 @@ app.post("/deletemedicine", async (req, res) => {
     const deleteQuery = "DELETE FROM prescription WHERE id = ?";
     const [results, fields] = await db.query(deleteQuery, [id]);
     if (results && results.affectedRows == 1) {
-      res.status(200).json({msg: "Medicine successfully deleted"});
+      return res.status(200).json({msg: "Medicine successfully deleted"});
     } else {
-      res.status(404).json({ "error": "Medicine not found" });
+      return res.status(404).json({ "error": "Medicine not found" });
     }
   } catch (error) {
     console.error(error);
-    res.status(500).json({ "error": "Internal server error" });
+    return res.status(500).json({ "error": "Internal server error" });
   }
 });
 
@@ -487,13 +512,13 @@ app.post("/viewmedicine", async (req, res) => {
     const selectQuery = `SELECT * FROM prescription WHERE user_id = ?`;
     const [results, fields] =  await db.query(selectQuery, [user_id]);
     if (results && results.length > 0) {
-      res.status(200).json(results);
+      return res.status(200).json(results);
     } else {
-      res.status(404).json({ "error": "No medicine found" });
+      return res.status(404).json({ "error": "No medicine found" });
     }
   } catch (error) {
     console.error(error);
-    res.status(500).json({ "error": "Internal server error" });
+    return res.status(500).json({ "error": "Internal server error" });
   }
 });
 
@@ -515,13 +540,13 @@ app.post("/logout", async (req, res) => {
     const updateQuery = "UPDATE session SET logout_time = NOW() WHERE id = ?";
     const [results, fields] = await db.query(updateQuery, [session_id]);
     if (results && results.affectedRows == 1) {
-      res.status(200).json({msg: "Logout successful"});
+      return res.status(200).json({msg: "Logout successful"});
     } else {
-      res.status(404).json({ "error": "Session not found" });
+      return res.status(404).json({ "error": "Session not found" });
     }
   } catch (error) {
     console.error(error);
-    res.status(500).json({ "error": "Internal server error" });
+    return res.status(500).json({ "error": "Internal server error" });
   }
 });
 
@@ -540,17 +565,24 @@ app.post("/emergencycontact/add", async (req, res) => {
 
   try {
     console.log("Adding emergency contact...");
-    const insertQuery = `INSERT INTO contact (user_id, first_name, last_name, phone, email)
-                          VALUES (?, ?, ?, ?, ?)`;
+    // const insertQuery = `INSERT INTO contact (user_id, first_name, last_name, phone, email)
+    //                       VALUES (?, ?, ?, ?, ?)`;
+    const insertQuery = `INSERT INTO contact (user_id, first_name, last_name, email, phone) 
+    VALUES (?, ?, ?, ?, ?)
+    ON DUPLICATE KEY UPDATE
+        first_name = VALUES(first_name),
+        last_name = VALUES(last_name),
+        phone = VALUES(phone),
+        email = VALUES(email);`;
     const [results, fields] =  await db.query(insertQuery, [user_id, first_name, last_name, phone, email]);
     if (results && results.affectedRows == 1) {
-      res.status(201).json({msg: "Emergency contact successfully added"});
+      return res.status(201).json({msg: "Emergency contact successfully added"});
     } else {
-      res.status(500).json({ "error": "Emergency contact addition failed" });
+      return res.status(500).json({ "error": "Emergency contact addition failed" });
     }
   } catch (error) {
      console.error(error);
-     res.status(500).json({ "error": "Internal server error" });
+     return res.status(500).json({ "error": "Internal server error" });
   }
 });
 
@@ -572,42 +604,15 @@ app.post("/emergencycontact", async (req, res) => {
     const selectQuery = `SELECT * FROM contact WHERE user_id = ?`;
     const [results, fields] =  await db.query(selectQuery, [user_id]);
     if (results && results.length > 0) {
-      res.status(200).json(results);
+      return res.status(200).json(results);
     } else {
-      res.status(404).json({ "error": "No emergency contact found" });
+      return res.status(404).json({ "error": "No emergency contact found" });
     }
   } catch (error) {
     console.error(error);
-    res.status(500).json({ "error": "Internal server error" });
+    return res.status(500).json({ "error": "Internal server error" });
   }
 });
 
-//--------------------------------------------------------------------------------------------------------------------------------
-/** Patient List
- * Show the list of patients for a caregiver
- * Frontend req: user_id
- * Backend res: Status code, all user information of patient(s) (first_name, last_name, email, phone, address_1, address_2, state, city, zip_code)
- * Postman Check - SUCCESS
- */ 
-app.post("/showpatients", async (req, res) => {
-  if (!req.body.user_id){
-    return res.status(400).json({ msg: "Missing the user_id"});
-  }
-
-  try {
-    const query = "SELECT user.* FROM account_link JOIN user ON account_link.patient_id = user.id WHERE account_link.caregiver_id = ?;";
-    const [results, fields] = await db.query(query, [req.body.user_id]);
-
-    if (results && results.length >= 1) {
-      console.log(results);
-      res.status(200).json(results);
-    } else {
-      res.status(204).json({ msg: "No patients for this user"});
-    }
-  } catch (error) {
-    console.error(error);
-    throw error;
-  }
-});
 
 module.exports = app; 
